f1cc7e0b4ad5effc544574fcaf6cc17a687be8c5
muhammedorhungunes <gunesmuhammedorhun@gmail.com>
2023-01-16 19:53:00 +0300

    add to git user info view components

---
+++ src/components/GitUserInfoView.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactJson from "react";
+
+class GitUserInfoView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { gitUser, fetching } = this.props.gitUser;
+
+    if (fetching) {
+      return <div>Yükleniyor...</div>;
+    } else {
+      return (
+        <div
+          style={{ borderColor: "blue", borderStyle: "solid", margin: "2px" }}
+        >
+          Git User Info
+          <div style={{ border: "2px solid #fdafda" }}>
+            <pre>{JSON.stringify(gitUser, null, 2)}</pre>
+          </div>
+          <div style={{ border: "2px solid #cdafda" }}>
+            <ReactJson src={gitUser} />
+          </div>
+        </div>
+      );
+    }
+  }
+}
+export default GitUserInfoView;
