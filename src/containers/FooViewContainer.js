0e9b24356606fabaadf3521834d3dd2f41923491
muhammedorhungunes <gunesmuhammedorhun@gmail.com>
2023-01-16 20:23:03 +0300

    add to foo view container

---
+++ src/containers/FooViewContainer.js
@@ -0,0 +1,12 @@
+import {connect} from 'react-redux';
+import FooView from '../components/FooView';
+
+const mapStateToProps = state => ({
+    visibilityMode : state.visibilityMode
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default FooViewContainer = connect(mapStateToProps,mapDispatchToProps)(FooView)
